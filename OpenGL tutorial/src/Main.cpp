ba467b80a54a738deaec433c1e4199272483dd5f
Knutt <knutti24@gmail.com>
2018-04-26 14:09:20 +0300

    Add shader class. Add simple vertex and fragment shaders

--- OpenGL tutorial/src/Main.cpp
+++ OpenGL tutorial/src/Main.cpp
@@ -1,20 +1,29 @@
 #include "glad\glad.h"
 #include "GLFW\glfw3.h"
 
+#include "shader.h"
+
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void process_input(GLFWwindow* window);
 
 int SCREEN_WIDTH = 800;
 int SCREEN_HEIGHT = 600;
 
+float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	0.5f, -0.5f, 0.0f,
+	0.0f,  0.5f, 0.0f
+};
+
 int main()
 {
+	//Initialize GLFW and create window
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
 
 	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "LearnOpenGL", NULL, NULL);
 	if (window == NULL)
@@ -26,17 +35,30 @@ int main()
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	//Load GLAD
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
 
+	unsigned int VBO;// VAO, EBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	Shader ourShader("shaders/shader.vs", "shaders/shader.fs");
+	ourShader.use();
+
 	//Render loop
 	while (!glfwWindowShouldClose(window))
 	{
+		process_input(window);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
+
+		ourShader.use();
 	}
 
 	glfwTerminate();
@@ -46,4 +68,10 @@ int main()
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
+}
+
+void process_input(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
 }
